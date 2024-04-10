55ae1d20245aad2394bb9140929c5a04b7e76943
Jan Dvorak <storytellercz@proton.me>
2024-04-10 14:00:07 +0200

    Published storyteller:cdn@2.1.2.

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
   name: 'storyteller:cdn',
-  version: '2.1.1',
+  version: '2.1.2',
   summary: 'Serve Meteor content from a CDN',
   git: 'https://github.com/StorytellerCZ/meteor-cdn.git',
   documentation: 'README.md'
 })
 
 Package.onUse(function (api) {
-  api.versionsFrom(['2.8.0', '3.0-beta.0'])
+  api.versionsFrom(['2.8.0', '3.0-beta.7'])
   api.export('CDN', 'server')
   api.export('CDN', 'client')
   api.use('webapp', 'server')
